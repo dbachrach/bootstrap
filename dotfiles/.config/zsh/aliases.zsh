ca69b600d9f2b9cfe2e04e999e2896aec30fd448
Dustin Bachrach <ahdustin@gmail.com>
2026-04-10 14:00:14 -0500

    aliases

--- dotfiles/.config/zsh/aliases.zsh
+++ dotfiles/.config/zsh/aliases.zsh
@@ -1,9 +1,24 @@
+# shell
 alias ll="ls -alh"
 alias o="open ."
-alias g="git"
-alias gst="git status"
 alias ..='cd ..'
 alias ...='cd ../..'
 alias ....='cd ../../..'
 alias .....='cd ../../../..'
+
+
+# git
+alias g="git"
+alias gst="git status"
+alias gcm="git commit -m"
+alias gcma="git commit -am"
+alias gaa="git add --all"
+alias gpush="git push"
+
+# workmux
 alias wm='workmux'
+
+## viteplus
+alias vpd="vp dev"
+alias vpt="vp test"
+alias vpc="vp check"
